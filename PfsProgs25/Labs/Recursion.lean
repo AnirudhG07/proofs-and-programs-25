ebcef7c789c7885b5a79dfcdac6e729e3b0d2b74
AnirudhG07 <anirudhgupta@iisc.ac.in>
2025-01-30 14:36:56 +0530

    lab 1,2

--- PfsProgs25/Labs/Recursion.lean
+++ PfsProgs25/Labs/Recursion.lean
@@ -1,4 +1,4 @@
-import Mathlib
+import Mathlib.Tactic
 
 /-!
 # Recursion
@@ -20,30 +20,37 @@ Complete the following definition:
 def fib (n : ℕ): ℕ :=
   match n with
   -- Change the code below (3 marks)
-  | _ => sorry
+  | 0 => 1
+  | 1 => 1
+  | n + 2 => fib (n + 1) + fib n
 
 /-! Test your definition with the following (uncomment the line below).-/
--- example : fib 7 = 21 := by rfl
+example : fib 7 = 21 := by rfl
 
 /-!
 ## Proving termination
 
 In the following, replace `partial def` by `def` and prove termination by choosing an appropriate function that decreases (2 marks).
 -/
-partial def f (a b : Nat) : Nat :=
+def f (a b : Nat) : Nat :=
   match a, b with
   | k + 2, l => f k (l + 1) + 1
   | k, l + 2 => f (k + 1) l + 2
   | _, _ => 1
+termination_by (a+b)
 
 /-!
 ## Proving termination
 
 In the following, replace `partial def` by `def` and prove termination by choosing an appropriate function that decreases and proving the appropriate lemma (4 marks).
 -/
-partial def numSteps (step: Nat → Nat)(h : ∀n: Nat, step (n + 1) ≤ n)
+
+def numSteps (step: Nat → Nat)(h : ∀n: Nat, step (n + 1) ≤ n)
     (n: Nat) : Nat :=
   match n with
   | 0 => 0
   | n + 1 =>
+    have h₁ : step (n + 1) ≤ n := by
+      apply h
     numSteps step h (step (n + 1)) + 1
+termination_by n
